65c095d454a90975b47db8bf87165a9048d7418c
IlSelva <silvioppastore@gmail.com>
2025-01-30 21:47:47 +0100

    handling chat rooms - server + partial controller

--- apps/chatServer/src/chatServer_controller.erl
+++ apps/chatServer/src/chatServer_controller.erl
@@ -4,39 +4,85 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start/0]).
 
+% users: a dictionary of Nickname:Socket
+% rooms: a dictionary of RoomName:list of User in the room
+%TODO rooms must contain also the room owner (creator's nickname). I could do rooms as a dictionari of
+% RoomName:record(list of users, owner). So that is easy to check if a room exists and access it.
+-record(state, {
+    users,
+    rooms}).
+
 start() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 % This is called when a connection is made to the server
 init([]) ->
-    Users = dict:new(), % It maps nick to socket
-    {ok, Users}.
+    State = #state{users = dict:new(), rooms = dict:new()},
+    {ok, State#state.users}.
 
 % handle_call is invoked in response to gen_server:call
-handle_call({connect, Nick, Socket}, _From, Users) ->
+handle_call({connect, Nick, Socket}, _From, State) ->
+    Users = State#state.users,
     Response = case dict:is_key(Nick, Users) of
         true ->
-            NewUsers = Users,
+            NewState = State,
             nick_in_use;
         false ->
             NewUsers = dict:append(Nick, Socket, Users),
+            NewState = State#state{users = NewUsers},
             {ok, user_list(NewUsers)}
     end,
-    {reply, Response, NewUsers};
+    {reply, Response, NewState};
 
-handle_call({disconnect, Nick}, _From, Users) ->
+handle_call({disconnect, Nick}, _From, State) ->
+    Users = State#state.users,
     Response = case dict:is_key(Nick, Users) of
         true ->
             NewUsers = dict:erase(Nick, Users),
+            NewState = State#state{users = NewUsers},
             ok;
         false ->
-            NewUsers = Users,
+            NewState = State,
             user_not_found
     end,
-    {reply, Response, NewUsers};
+    {reply, Response, NewState};
 
-handle_call({list_users}, _From, Users) ->
-    {reply, user_list(Users), Users};
+handle_call({list_users}, _From, State) ->
+    {reply, user_list(State#state.users), State};
+
+handle_call({list_rooms}, _From, State) ->
+    {reply, room_list(State#state.rooms), State};
+
+handle_call({create_room, Nick, Name}, _From, State) ->
+    Rooms = State#state.rooms,
+    Response = case dict:is_key(Name, Rooms) of
+                   true ->
+                       NewState = State,
+                       {error, "Room: "++ Name++ " already exists"};
+                   false ->
+                       NewRooms = dict:store(Name,[], Rooms),
+                       NewState = State#state{rooms = NewRooms},
+                       ok
+               end,
+    {reply, Response, NewState};
+
+handle_call({destroy_room}, _From, State) ->
+    %TODO add room owner (creator)
+    {};
+
+handle_call({join_room, Nick, Name}, _From, State) ->
+    Rooms = State#state.rooms,
+    Response = case dict:is_key(Name, Rooms) of
+                 true ->
+                   %check if Nick is already in room
+                    NewRooms = dict:append(Name, Nick, Rooms),
+                    NewState = State#state{rooms = NewRooms},
+                   ok;
+                 false ->
+                   NewState = State,
+                   {error, "Room: "++ Name ++ "not found"}
+               end,
+  {reply, Response, NewState};
 
 handle_call(_Message, _From, State) ->
     {reply, error, State}.
@@ -58,6 +104,7 @@ handle_cast(_Message, State) ->
 
 % auxiliary functions
 broadcast(Nick, Msg, Users) ->
+    %%remove the calling user from the dictionary before converting it to list
     Sockets = lists:map(fun({_, [Value|_]}) -> Value end, dict:to_list(dict:erase(Nick, Users))),
     lists:foreach(fun(Sock) -> gen_tcp:send(Sock, Msg) end, Sockets).
 
--- apps/chatServer/src/chatServer_server.erl
+++ apps/chatServer/src/chatServer_server.erl
@@ -52,14 +52,24 @@ handle_command(Nick, Socket) ->
                     case clean(Message) of
                         "LIST" ->
                             list(Nick, Socket);
+                        "LISTROOM" ->
+                            list_room(Nick, Socket);
                         "QUIT" ->
                             quit(Nick, Socket)
                         end;
                 true ->
                     {Command, [_|Content]} = lists:splitwith(fun(T) -> [T] =/= ":" end, Message),
                     case Command of
-                        "QUIT" ->
-                            quit(Nick, Socket)
+                        "CREATE" ->
+                            create_room(Nick, Socket, clean(Content));
+                        "DESTROY" ->
+                            destroy_room(Nick, Socket, clean(Content));
+                        "JOIN" ->
+                            join_room(Nick, Socket, clean(Content));
+                        "LEAVE" ->
+                            leave_room(Nick, Socket, clean(Content));
+                        "SAY" ->
+                            say(Nick, Socket, clean(Content));
                     end
             end;
         {error, closed} ->
@@ -69,6 +79,11 @@ handle_command(Nick, Socket) ->
 list(Nick, Socket) ->
     Users = gen_server:call(?CONTROLLER,  {list_users}),
     gen_tcp:send(Socket, "Users list: "++ Users ++ "\n"),
+    handle_command(Nick, Socket).
+
+list_room(Nick, Socket) ->
+    Rooms = gen_server:call(?CONTROLLER, {list_rooms}),
+    gen_tcp:send(Socket, "Room list: "++ Rooms ++ "\n" ),
     handle_command(Nick, Socket).
 
 quit(Nick, Socket) ->
@@ -81,7 +96,60 @@ quit(Nick, Socket) ->
         user_not_found ->
             gen_tcp:send(Socket, "Bye with errors.\n"),
             ok
-    end.
+    end,
+    handle_command(Nick, Socket).
+
+create_room(Nick, Socket, Content) ->
+    Response = gen_server:call(?CONTROLLER, {create_room, Nick, Content}),
+    case Response of
+        ok ->
+            gen_tcp:send(Socket, "CREATE_ROOM:OK:"++ Content ++" created"),
+            ok;
+        {error, MESSAGE} ->
+            gen_tcp:send(Socket, "CREATE_ROOM:ERROR:"++ MESSAGE),
+            ok
+    end,
+    handle_command(Nick, Socket).
+
+destroy_room(Nick, Socket, Content) ->
+    Response = gen_server:call(?CONTROLLER, {destroy_room, Nick, Content}),
+    case Response of
+        ok ->
+            gen_tcp:send(Socket, "DESTROY_ROOM:OK:"++ Content ++" destoryed correctly"),
+            ok;
+        {error, MESSAGE} ->
+            gent_tcp:send(Socket, "DESTROY_ROOM:ERROR:"++ MESSAGE),
+            ok
+    end,
+    handle_command(Nick, Socket).
+
+join_room(Nick, Socket, Content) ->
+    Response = gen_server:call(?CONTROLLER, {join_room, Nick, Content}),
+    case Response of
+        ok ->
+            gen_tcp:send(Socket, "JOIN_ROOM:OK:" ++ Content ++ " joined"),
+            ok;
+        {error, MESSAGE} ->
+            gen_tcp:send(Socket, "JOIN_ROOM:ERROR:" ++ MESSAGE),
+            ok
+    end,
+    handle_command(Nick, Socket).
+
+leave_room(Nick, Socket, Content) ->
+    Response = gen_server:call(?CONTROLLER, {leave_room, Nick, Content}),
+    case Response of
+        ok ->
+            gen_tcp:send(Socket, "LEAVE_ROOM:OK:" ++ Content ++ " left"),
+            ok;
+        {error, MESSAGE} ->
+            gen_tcp:send(Socket, "LEAVE_ROOM:ERROR:" ++ MESSAGE),
+            ok
+    end,
+    handle_command(Nick, Socket).
+
+say(Nick, Socket, Content) ->
+    gen_server:cast(?CONTROLLER, {say, Nick, Content}),
+    handle_command(Nick, Socket).
 
 clean(Data) ->
     string:trim(Data).
